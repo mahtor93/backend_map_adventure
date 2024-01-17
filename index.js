61b2daeedf82d0df54cd6720d1a07c2139170a57
Mario <mh.torneria@gmail.com>
2024-01-17 16:42:12 -0300

    prodgit add .!

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ app.use(cors(
 ));
 
 app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin', 'https://map-adventure.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
