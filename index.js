f25caeb297c121a982d2b27a45c254187dbe0e60
Mario <mh.torneria@gmail.com>
2024-01-17 16:12:35 -0300

    ajustes CORS

--- index.js
+++ index.js
@@ -8,13 +8,23 @@ dotenv.config({
     path:'.env'
   });
 
-  const allowedOrigins = [
+const allowedOrigins = [
     process.env.FRONTEND_DEV_URL,
     process.env.FRONTEND_PROD_URL
   ];
 app.use(cors(
-    {origin: allowedOrigins}
+    {
+      origin: allowedOrigins,
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    }
 ));
+
+app.use((req,res,next)=>{
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/', router);
 
