44a50a654f9407216ecb2036f41bae78daee662a
Mario <mh.torneria@gmail.com>
2024-01-17 16:34:02 -0300

    trabajo con prod_url

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors(
 ));
 
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', `${process.env.FRONTEND_DEV_URL}, ${process.env.FRONTEND_PROD_URL}`);
+  res.header('Access-Control-Allow-Origin', `${process.env.FRONTEND_PROD_URL}`);
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
