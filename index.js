c9add8d2e98e31c514422244a1688de854da8149
Mario <mh.torneria@gmail.com>
2024-01-17 16:45:04 -0300

    localhost

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors(
 ));
 
 app.use((req,res,next)=>{
-  res.header('Access-Control-Allow-Origin', 'https://map-adventure.vercel.app');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
